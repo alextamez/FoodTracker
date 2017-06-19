60bd83be3e132f8d4b79a485bf1c3e9c30a6691d
Alex Tamez <alextamez@users.noreply.github.com>
2017-06-19 17:48:09 -0500

    Files for steps 1 & 2
    - Build a Basic UI
    - Connect the UI to Code

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Alejandro Tamez on 16/06/17.
+//  Copyright © 2017 EvolucionMarketing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+
+    //MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+    
+    
+}
+
